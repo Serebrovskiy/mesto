9ef8ab17349b7dd6185a9e3eb92e4cd63d7f3ee8
Aleksandr Serebrovskiy <aleksandr-web@yandex.ru>
2020-05-27 21:28:52 +0400

    sent to review

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// попап изменения профиля
-const popup = document.querySelector('.popup')
+// для изменения профиля
+const popupProfile = document.querySelector('.popup')
 const change = document.querySelector('.popup__container');
 const buttonOpen = document.querySelector('.profile__edit');
 const buttonClose = document.querySelector('.popup__close-icon');
@@ -8,49 +8,73 @@ const jobInput = document.querySelector('.popup__input-text_type_job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__profession');
 
-// попап добавления карточек
+// для попапа добавления карточек
 const popupCard = document.querySelector('.popup_add_card');
 const buttonOpenCard = document.querySelector('.profile__button');
 const placeInput = document.querySelector('.popup__input-text_type_place');
 const imageInput = document.querySelector('.popup__input-text_type_image');
 const changeCard = document.querySelector('.popup__container_add_card');
 const buttonCloseCard = document.querySelector('.popup__close-icon_add_card');
+const cards = document.querySelector('.cards');
+const cardsTemplate = document.querySelector('#card').content;
 
-// попап просмотра картинок
+// для попапа просмотра картинок
 const popupViewImage = document.querySelector('.popup_view-image');
 const buttonCloseImage = document.querySelector('.popup__close-icon_image');
 const captionImage = document.querySelector('.popup__caption');
 const popupImage = document.querySelector('.popup__image');
 
-//работа с DOM
-const cardPlace = document.querySelector('.elements');
-const cardTemplate = document.querySelector('#card').content;
-const elements = document.querySelector('.elements');
-
 // попап изменения профиля
-function openPopup() {
+function openPopupProfile() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-  popup.classList.toggle('popup_opened');
-
-  function closePopup() {
-    popup.classList.remove('popup_opened');    // это хорошо бы исправить, сделать одну функцию toogle()  и вообще исправить всю функцию
-  }
-
-  buttonClose.addEventListener('click', closePopup);
-
-  function changeElements(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    closePopup();
-  }
-
-  change.addEventListener('submit', changeElements);
+  toggleClassProfile();
 }
-buttonOpen.addEventListener('click', openPopup);
 
+function changeElements(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  toggleClassProfile();
+}
 
+buttonOpen.addEventListener('click', openPopupProfile);
+change.addEventListener('submit', changeElements);
+buttonClose.addEventListener('click', toggleClassProfile);
+
+// попап добавления карточек
+function openPopupCard() {
+  imageInput.value = '';
+  placeInput.value = '';
+  toggleClassCard();
+}
+
+function changeElementsCard(evt) {
+  evt.preventDefault();
+  addCard(placeInput.value, imageInput.value);
+  toggleClassCard();
+}
+
+//открываем/закрываем попап изменения профиля
+function toggleClassProfile() {
+  popupProfile.classList.toggle('popup_opened');
+}
+
+//открываем/закрываем попап с карточками
+function toggleClassCard() {
+  popupCard.classList.toggle('popup_opened');
+}
+
+//открываем/закрываем попап просмотра картинок
+function toggleClassImage() {
+  popupViewImage.classList.toggle('popup_opened');
+}
+
+buttonOpenCard.addEventListener('click', openPopupCard);
+changeCard.addEventListener('submit', changeElementsCard);
+buttonCloseCard.addEventListener('click', toggleClassCard);
+
+//исходный масив
 const initialCards = [
   {
     name: 'Смоленск',
@@ -78,73 +102,44 @@ const initialCards = [
   }
 ];
 
-// попап добавления карточек
-function openPopupCard() {
-  imageInput.value = '';
-  placeInput.value = '';
-  toggleClassCard();
-}
+//добавляем исходные карточки
+initialCards.forEach(elem => {
+  addCard(elem.name, elem.link);
+});
 
-function changeElementsCard(evt) {
-  evt.preventDefault();
+//добавление карточкy
+function addCard(place, image) {
+  const cardElements = cardsTemplate.cloneNode(true);
+  cardElements.querySelector('.card__image').src = image;
+  cardElements.querySelector('.card__title').textContent = place;
+  cardElements.querySelector('.card__image').alt = place;
 
-  addCards2(placeInput.value, imageInput.value);
-  initialCards.push({ name: placeInput.value, link: imageInput.value });  //добавляем объект в массив
-
-  toggleClassCard();
-}
-
-function toggleClassCard() {
-  popupCard.classList.toggle('popup_opened');   //открываем попап с карточками
-}
-
-function toggleClassImage() {
-  popupViewImage.classList.toggle('popup_opened');    //открываем попап просмотра картинок
-}
-
-buttonOpenCard.addEventListener('click', openPopupCard);
-changeCard.addEventListener('submit', changeElementsCard);
-buttonCloseCard.addEventListener('click', toggleClassCard);
-
-//добавление карточки
-function addCards2(place, image) {
-  const cardElement = cardTemplate.cloneNode(true);
-  cardElement.querySelector('.element__image').src = image;
-  cardElement.querySelector('.element__title').textContent = place;
-  cardElement.querySelector('.element__image').alt = place;
-
-  cardElement.querySelector('.element__basket').addEventListener('click', function (evt) {  //удаляем карточку
+  //удаляем карточку
+  cardElements.querySelector('.card__basket').addEventListener('click', function (evt) {
     evt.target.parentElement.remove();
   });
 
-  cardElement.querySelector('.element__like').addEventListener('click', function (evt) {  //ставим лайк
-    evt.target.classList.toggle('element__like_active');
+  //ставим лайк
+  cardElements.querySelector('.card__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('card__like_active');
   });
 
-  cardElement.querySelector('.element__image').addEventListener('click', function (evt) {  //забираем данные с о картинке
-    captionImage.textContent = evt.target.parentElement.querySelector('.element__title').textContent;
-    popupImage.src = evt.target.parentElement.querySelector('.element__image').src;
+  //забираем данные о картинке
+  cardElements.querySelector('.card__image').addEventListener('click', function (evt) {
+    captionImage.textContent = evt.target.parentElement.querySelector('.card__title').textContent;
+    popupImage.src = evt.target.parentElement.querySelector('.card__image').src;
+    popupImage.alt = evt.target.parentElement.querySelector('.card__title').textContent;
 
     toggleClassImage();
   });
 
-  elements.prepend(cardElement);
+  cards.prepend(cardElements);
 }
 
 buttonCloseImage.addEventListener('click', toggleClassImage);
 
-initialCards.forEach(elem => {
-  addCards2(elem.name, elem.link);
-});
 
 
 
 
 
-
-
-  //initialCards.splice(initialCards.findIndex(item => item.field === place), 2);
-  // const crow = initialCards.find(function (bird) {
-  //   return bird.includes("place");
-//});
-//console.log(crow);
